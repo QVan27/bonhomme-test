491f1a4bc426f4e81e9365fbc9494dd29b3d6fef
Quentin Vannarath <qvannarath@gmail.com>
2024-04-27 12:01:00 +0200

    ✨[HEADER] Template - Style - RWD

--- themes/artvannah/resources/views/partials/header.blade.php
+++ themes/artvannah/resources/views/partials/header.blade.php
@@ -1,4 +1,30 @@
 <header class="header">
-  <div class="container-fluid">
-  </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-22 offset-1 col-lg-18 offset-lg-3">
+                <div class="header__container u-flex-between">
+                    <a class="u-upper header__logo" href="{{ home_url() }}">
+                        {{ display_svg('four-dots') }}
+                        menu
+                    </a>
+                    <nav>
+                        <ul>
+                            @php
+                                $menu = [
+                                    '#popular' => 'Popular',
+                                    '#now-showing' => 'Now Showing',
+                                    '#watchlist' => 'Watchlist',
+                                ];
+                            @endphp
+                            @foreach ($menu as $slug => $name)
+                                <li>
+                                    <a class="u-upper" href="{{ home_url($slug) }}">{{ $name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
